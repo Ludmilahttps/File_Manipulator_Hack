827a56a6a981eb677237bebe37237bd84b70b7c8
Ludmilahttps <ludmila.https@gmail.com>
2022-09-27 22:12:42 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << ------------------------------------------------- << endl
+        << FILE MANIPULATOR HACK << endl
+        << Select an option:
+        << 1. Open a File...
+        << 2. Search for Substrings...
+        << 3. Remove Words Containing a Substring
+        << 4. Remove all Repeated Words
+        << 5. Show Statistics
+        << 6. Exit
+        << -------------------------------------------------
+        << Option: << endl;
+
+    return 0;
+}
