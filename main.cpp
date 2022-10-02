914fb66637899b3e6fe31a1ec1ab8ac02ef031d6
Ludmilahttps <ludmila.https@gmail.com>
2022-10-02 00:06:35 -0300

    done*

--- main.cpp
+++ main.cpp
@@ -6,15 +6,16 @@
 #include <sstream>
 #include <algorithm>
 
-using namespace std;
+using namespace std; // ¯\(°_o)/¯
 
 size_t menu();
-size_t open_file(string filename, vector<string> &liststrings);
+bool open_file(string filename, vector<string> &liststrings);
 size_t search_substring(vector<pair<string, vector<string>>> &ListOfFiles, string search, size_t option);
 void remove_duplicates_files(vector<pair<string, vector<string>>> &ListOfFiles);
 void remove_duplicates_words(vector<pair<string, vector<string>>> &ListOfFiles);
 size_t statistics(const vector<pair<string, vector<string>>> ListOfFiles);
 size_t save_file(string Outfilename, vector<pair<string, vector<string>>> &ListOfFiles);
+size_t printStrings(vector<pair<string, vector<string>>> &ListOfFiles);
 
 int main()
 {
@@ -33,13 +34,15 @@ int main()
                  << "Enter a file to open: " << endl;
             cin >> filename;
 
-            open_file(filename, liststrings);
+            if (open_file(filename, liststrings))
+            {
 
-            file.first = filename;
-            file.second = liststrings;
+                file.first = filename;
+                file.second = liststrings;
 
-            ListOfFiles.push_back(file);
-            liststrings.clear();
+                ListOfFiles.push_back(file);
+                liststrings.clear();
+            }
 
             continue;
         }
@@ -79,26 +82,32 @@ int main()
         }
         if (option == 6)
         {
-            cout << "5. Show Statistics" << endl;
+            cout << "6. Show Statistics" << endl;
             cout << "File Manipulator Hack Statistics:" << endl;
             statistics(ListOfFiles);
             continue;
         }
         if (option == 7)
         {
-            cout << "6. Exit" << endl
+            cout << "7. Print strings" << endl;
+            // printStrings(ListOfFiles);
+            continue;
+        }
+        if (option == 8)
+        {
+            cout << "8. Exit" << endl
                  << "Enter a name to save the file: " << endl;
             string filename;
             cin >> filename;
             save_file(filename, ListOfFiles);
             break;
         }
-        if (option != 1 || option != 2 || option != 3 || option != 4 || option != 5 || option != 6 || option != 7)
+        if (option != 1 || option != 2 || option != 3 || option != 4 || option != 5 || option != 6 || option != 7 || option != 8)
         {
             cout << "Please chose a valid number" << endl;
             continue;
         }
-    } while (option != 7);
+    } while (option != 8);
     return 0;
 }
 
@@ -114,7 +123,8 @@ size_t menu()
          << "4. Remove all Repeated Words" << endl
          << "5. Remove all Repeated Files" << endl
          << "6. Show Statistics" << endl
-         << "7. Exit" << endl
+         << "7. Print strings (off)" << endl
+         << "8. Exit" << endl
          << "-------------------------------------------------" << endl
          << "Option:";
     cin >> option;
@@ -122,7 +132,7 @@ size_t menu()
     return option;
 }
 
-size_t open_file(string filename, vector<string> &liststrings)
+bool open_file(string filename, vector<string> &liststrings)
 {
     ifstream infile(filename); // abre o arquivo em questão para pegar as strings
 
@@ -138,11 +148,7 @@ size_t open_file(string filename, vector<string> &liststrings)
             {
                 string element;
                 ss_line >> element;
-                if (element.size() == 0)
-                {
-                    // retira os espaços vazios
-                }
-                else
+                if (element.size() != 0)
                 {
                     liststrings.push_back(element);
                 }
@@ -150,8 +156,13 @@ size_t open_file(string filename, vector<string> &liststrings)
         }
 
         infile.close(); // fecha o arquivo
+        return true;
     }
-    return 0;
+    else
+    {
+        cout << "File not found" << endl;
+        return false;
+    }
 }
 
 size_t search_substring(vector<pair<string, vector<string>>> &ListOfFiles, string search, size_t option)
@@ -179,7 +190,6 @@ size_t search_substring(vector<pair<string, vector<string>>> &ListOfFiles, strin
                     {
                         cout << "Removing " << ListOfFiles.at(i).second.at(j) << " located in file " << ListOfFiles.at(i).first << endl;
                         ListOfFiles.at(i).second.at(j).erase();
-                        ListOfFiles.at(i).second.erase((ListOfFiles.at(i).second.begin(), ListOfFiles.at(i).second.end() + i));
                     }
                 }
             }
@@ -232,7 +242,18 @@ size_t statistics(const vector<pair<string, vector<string>>> ListOfFiles)
     {
         for (size_t i = 0; i < ListOfFiles.size(); i++)
         {
-            cout << "File " << ListOfFiles.at(i).first << " has " << ListOfFiles.at(i).second.size() << " words." << endl;
+            size_t sp = 0;
+            for (size_t j = 0; j < ListOfFiles.at(i).second.size(); j++)
+            {
+
+                if (ListOfFiles.at(i).second.at(j).length() == 0)
+                {
+
+                    sp++;
+                }
+            }
+            cout << "File " << ListOfFiles.at(i).first << " has " << ListOfFiles.at(i).second.size() - sp << " words." << endl;
+            sp = 0;
         }
     }
     return 0;
@@ -240,13 +261,29 @@ size_t statistics(const vector<pair<string, vector<string>>> ListOfFiles)
 
 size_t save_file(string OutFilename, vector<pair<string, vector<string>>> &ListOfFiles)
 {
-    ofstream OutFile(OutFilename);
+    ofstream OutFile(OutFilename + ".txt");
 
     for (size_t i = 0; i < ListOfFiles.size(); i++)
     {
         for (size_t j = 0; j < ListOfFiles.at(i).second.size(); j++)
         {
-            OutFile << ListOfFiles.at(i).first << "  " << ListOfFiles.at(i).second.at(j) << endl;
+            if (ListOfFiles.at(i).second.at(j).length() != 0)
+            {
+                OutFile << ListOfFiles.at(i).first << "  " << ListOfFiles.at(i).second.at(j) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
+
+size_t printStrings(vector<pair<string, vector<string>>> &ListOfFiles)
+{
+    for (size_t i = 0; i < ListOfFiles.size(); i++)
+    {
+        for (size_t j = 0; j < ListOfFiles.at(i).second.size(); j++)
+        {
+            cout << ListOfFiles.at(i).first << "  " << ListOfFiles.at(i).second.at(j) << endl;
         }
     }
 
